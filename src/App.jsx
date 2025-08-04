2b6a4a7a3e4a67be4b4a4eb2c7954f95102585f3
NjilaBoris <njilaboris2@gmail.com>
2025-08-04 23:46:02 +0100

    Setting Up Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,140 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "motion/react";
+import { useOnClickOutside } from "usehooks-ts";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GAMES = [
+  {
+    title: "The Oddysey",
+    description: "Explore unknown galaxies.",
+    longDescription:
+      "Throughout their journey, players will encounter diverse alien races, each with their own unique cultures and technologies. Engage in thrilling space combat, negotiate complex diplomatic relations, and make critical decisions that affect the balance of power in the galaxy.",
+    image:
+      "https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/how-i-code-animations/space.png",
+  },
+  {
+    title: "Angry Rabbits",
+    description: "They are coming for you.",
+    longDescription:
+      "The rabbits are angry and they are coming for you. You have to defend yourself with your carrot gun. The game is not simple, you have to be fast and accurate to survive.",
+    image:
+      "https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/how-i-code-animations/rabbit.png",
+  },
+  {
+    title: "Ghost town",
+    description: "Find the ghosts.",
+    longDescription:
+      "You are in a ghost town and you have to find the ghosts. But be careful, they are dangerous.",
+    image:
+      "https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/how-i-code-animations/ghost.webp",
+  },
+  {
+    title: "Pirates in the jungle",
+    description: "Find the treasure.",
+    longDescription:
+      "You are a pirate and you have to find the treasure in the jungle. But be careful, there are traps and wild animals.",
+    image:
+      "https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/how-i-code-animations/pirate.png",
+  },
 
+  {
+    title: "Lost in the mountains",
+    description: "Find your way home.",
+    longDescription:
+      "You are lost in the mountains and you have to find your way home. But be careful, there are dangerous animals and you can get lost.",
+    image:
+      "https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/how-i-code-animations/boy.webp",
+  },
+];
+const App = () => {
+  const [activeGame, setActiveGame] = useState(null);
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => setActiveGame(null));
+
+  useEffect(() => {
+    function onKeyDown(event) {
+      if (event.key === "Escape") {
+        setActiveGame(null);
+      }
+    }
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="relative h-dvh flex w-full min-w-[300px] items-center justify-center p-6">
+      {activeGame ? (
+        <>
+          <div className="pointer-events-none absolute inset-0 z-10 bg-neutral-300/20" />
+          <div className="absolute inset-0 z-10 grid place-items-center">
+            <div
+              className="flex h-fit max-w-[500px] cursor-pointer flex-col items-start gap-4 overflow-hidden border border-neutral-400 bg-white p-4"
+              ref={ref}
+              style={{ borderRadius: 12 }}
+            >
+              <div className="flex w-full items-center gap-4">
+                <span>
+                  <img
+                    alt="Game"
+                    src={activeGame.image}
+                    style={{ borderRadius: 12 }}
+                    className="h-14 w-14"
+                  />
+                </span>
 
-export default App
+                <div className="flex flex-grow items-center justify-between">
+                  <div className="flex flex-col py-3">
+                    <h2 className="text-sm font-semibold text-neutral-700">
+                      {activeGame.title}
+                    </h2>
+                    <p className="text-sm text-neutral-500">
+                      {activeGame.description}
+                    </p>
+                  </div>
+                  <button className="rounded-full bg-neutral-100 px-3 py-1 text-xs font-semibold text-blue-600">
+                    Get
+                  </button>
+                </div>
+              </div>
+              <p className="text-sm text-neutral-600">
+                {activeGame.longDescription}
+              </p>
+            </div>
+          </div>
+        </>
+      ) : null}
+
+      <ul className="relative my-12 flex w-full flex-col items-center p-0">
+        {GAMES.map((game) => (
+          <motion.li
+            key={game.title}
+            onClick={() => setActiveGame(game)}
+            style={{ borderRadius: 8 }}
+            className="flex w-full cursor-pointer items-center gap-4 p-0 sm:w-[368px]"
+          >
+            <span>
+              <img
+                alt="Game"
+                src={game.image}
+                style={{ borderRadius: 12 }}
+                priority
+                className="h-14 w-14"
+              />
+            </span>
+            <div className="flex flex-grow items-center justify-between border-b border-neutral-300">
+              <div className="flex flex-col py-4">
+                <h2 className="text-sm font-semibold text-neutral-700 ">
+                  {game.title}
+                </h2>
+                <p className="text-sm text-neutral-500">{game.description}</p>
+              </div>
+              <button className="rounded-full bg-neutral-100 px-3 py-1 text-xs font-semibold text-blue-600">
+                Get
+              </button>
+            </div>
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
